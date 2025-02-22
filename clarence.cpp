5f2ec63440b904e33951e79dc48be2a8bce131a7
ClarenceBaral <24-16459@g.batstate-u.edu.ph>
2025-02-22 11:50:50 +0800

    binary search

--- clarence.cpp
+++ clarence.cpp
@@ -6,6 +6,24 @@ using namespace std;
  int numElem = 0;
  int maxElem = 20;
 
+ void bubbleSort(){
+    int i, j;
+    bool swapped;
+    for(int i =0; i < numElem - 1; i++){
+        swapped = false;
+        for(j = 0; j < numElem - i -1; j++){
+            if(myArray[j] > myArray[j+1]){
+                swap(myArray[j], myArray[j+1]);
+                swapped = true;
+            }
+        }
+
+        if(swapped == false){
+            break;
+        }
+    }
+}
+
  void traverseArray() {
      cout << "showing elements in my array \n";
        for(int i = 0; i< maxElem; i++){
@@ -62,18 +80,126 @@ using namespace std;
      
      return "The array is full. Please remove elements before inserting new one. \n";
 }
+
+string insertAtSortedArray(int num){
+    if(numElem != maxElem){
+        bubbleSort();
+        int position;
+    for(int i = 0; i < numElem; i++){
+        if(num < myArray[i]){
+            position = i;
+            break;
+        }
+    }
+
+    for(int i = numElem; i>=position; i--){
+        myArray[i+1] = myArray[i];
+    }
+
+    myArray[position] = num;
+    numElem++;
+    return "An element is succesfully inserted at a sorted array! \n";
+    }
+
+    return "The Array is full. Please remove elements before inserting new one. \n";
+}
  
+ string deleteFromTheEnd(){
+    if(numElem != 0){
+        numElem--;
+        return "An element has been deleted from the end!";
+    }
+    return "The array is underflow! Delete is not possible";
+}
+
+string deleteFromTheBeginning(){
+    if(numElem != 0){
+        for(int i = 0; i<= numElem; i++ ){
+        myArray[i] = myArray[i+1];
+        }
+        numElem--;
+        return "An element has been deleted from the beginning!";
+    }
+    return "The array is underflow! Delete is not possible";
+}
+
+string deleteFromGivenElement(int element){
+    if(numElem != 0){
+        int index;
+        for(int i = 0; i <= numElem; i++){
+            if(myArray[i] == element){
+                index = i;
+                break;
+            }
+        }
+
+        for(int i = index; i <= numElem; i++){
+            myArray[i] = myArray[i+1];
+        }
+        numElem--;
+        return to_string(element) + "has been deleted!";
+
+    }
+    return "The array is underflow! Deleted is not possible";
+    
+}
+
+int binarySearch(int num, int lowest, int highest){
+    while(lowest <= highest){
+        int middle = lowest + (highest - lowest) / 2;
+
+        if(myArray[middle] ==num){
+            lowest = middle + 1;
+        }else{
+            highest = middle - 1;
+        }
+    }
+    return -1;
+}
+
 int main(){
     
-    insertAtTheBeginning(3);
-    insertAtTheEnd(7);
-    insertAtTheEnd(9);
-    insertAtTheBeginning(20);
-    insertAtTheGivenPosition(37, 7);
-    insertAtTheBeginning(256);
-    insertAtTheGivenPosition(128, 9);
-    insertAtTheBeginning(19);
+   cout << insertAtTheBeginning(3);
+    traverseArray();
+   cout << insertAtTheEnd(7);
+    traverseArray();
+   cout << insertAtTheEnd(9);
+    traverseArray();
+   cout << insertAtTheBeginning(20);
+    traverseArray();
+   cout << insertAtTheGivenPosition(37, 7);
+    traverseArray();
+   cout << insertAtTheBeginning(256);
+    traverseArray();
+   cout << insertAtTheGivenPosition(128, 9);
+    traverseArray();
+   cout << insertAtTheBeginning(19);
     traverseArray();
     
+   cout << insertAtSortedArray(3);
+    traverseArray();
+   cout << insertAtSortedArray(30);
+    traverseArray();
+   cout << insertAtSortedArray(38);
+    traverseArray();
+   cout << insertAtSortedArray(19);
+    traverseArray();
+
+   cout << deleteFromTheBeginning();
+   traverseArray();
+   cout << deleteFromTheBeginning();
+    traverseArray();
+   cout << deleteFromTheEnd();
+    traverseArray();
+   cout << deleteFromTheEnd();
+    traverseArray();
+   // cout << deleteFromGivenElement(20);
+   // cout << deleteFromGivenElement(38);
+
+    traverseArray();
+
+    cout << binarySearch (20, 0, numElem);
+    cout << binarySearch (38, 0, numElem);
+
     return 0;
 }
